5cdbd399273bb380eda9be11d6ebcc566647e5ee
SatpalInfilogix <satpalinfilogix@gmail.com>
2024-07-25 11:45:51 +0530

    Removed unwanted images and updated loggedin username, role & profile picture

--- resources/views/admin/includes/header.blade.php
+++ resources/views/admin/includes/header.blade.php
@@ -1,9 +1,15 @@
 <div class="header">
 
     <div class="header-left active">
-        <a href="{{ route('backend-dashboard') }}" class="logo logo-normal"><img src="{{ asset('assets/img/logo.png') }}" alt></a>
-        <a href="{{ route('backend-dashboard') }}" class="logo logo-white"><img src="{{ asset('assets/img/logo-white.png') }}" alt></a>
-        <a href="{{ route('backend-dashboard') }}" class="logo-small"><img src="{{ asset('assets/img/logo-small.png') }}" alt></a>
+        <a href="{{ route('backend-dashboard') }}" class="logo logo-normal">
+            <img src="{{ asset('assets/img/logo.png') }}" alt>
+        </a>
+        <a href="{{ route('backend-dashboard') }}" class="logo logo-white">
+            <img src="{{ asset('assets/img/logo-white.png') }}" alt>
+        </a>
+        <a href="{{ route('backend-dashboard') }}" class="logo-small">
+            <img src="{{ asset('assets/img/logo-small.png') }}" alt>
+        </a>
         <a id="toggle_btn" href="javascript:void(0);">
             <i data-feather="chevrons-left" class="feather-16"></i>
         </a>
@@ -18,237 +24,57 @@
     </a>
 
     <ul class="nav user-menu">
+        <li class="nav-item nav-searchinputs"></li>
 
-        <li class="nav-item nav-searchinputs">
-           {{--  <div class="top-nav-search">
-                <a href="javascript:void(0);" class="responsive-search">
-                    <i class="fa fa-search"></i>
-                </a>
-                <form action="#" class="dropdown">
-                    <div class="searchinputs dropdown-toggle" id="dropdownMenuClickable"
-                        data-bs-toggle="dropdown" data-bs-auto-close="false">
-                        <input type="text" placeholder="Search">
-                        <div class="search-addon">
-                            <span><i data-feather="x-circle" class="feather-14"></i></span>
-                        </div>
-                    </div>
-                    <div class="dropdown-menu search-dropdown" aria-labelledby="dropdownMenuClickable">
-                        <div class="search-info">
-                            <h6><span><i data-feather="search" class="feather-16"></i></span>Recent Searches
-                            </h6>
-                            <ul class="search-tags">
-                                <li><a href="javascript:void(0);">Products</a></li>
-                                <li><a href="javascript:void(0);">Sales</a></li>
-                                <li><a href="javascript:void(0);">Applications</a></li>
-                            </ul>
-                        </div>
-                        <div class="search-info">
-                            <h6><span><i data-feather="help-circle" class="feather-16"></i></span>Help</h6>
-                            <p>How to Change Product Volume from 0 to 200 on Inventory management</p>
-                            <p>Change Product Name</p>
-                        </div>
-                        <div class="search-info">
-                            <h6><span><i data-feather="user" class="feather-16"></i></span>Customers</h6>
-                            <ul class="customers">
-                                <li>
-                                    <a href="javascript:void(0);">Aron Varu<img src="{{ asset('assets/img/profiles/avator1.jpg') }}" alt class="img-fluid"></a>
-                                </li>
-                                <li>
-                                    <a href="javascript:void(0);">Jonita<img src="{{ asset('assets/img/profiles/avatar-01.jpg') }}" alt class="img-fluid"></a>
-                                </li>
-                                <li>
-                                    <a href="javascript:void(0);">Aaron<img src="{{ asset('assets/img/profiles/avatar-10.jpg') }}" alt class="img-fluid"></a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </form>
-            </div> --}}
-        </li>
+        <a href="{{ route('pos-dashboard') }}" target="_blank"><strong>POS Dashboard</strong></a>
 
-
-        <!-- <li class="nav-item dropdown has-arrow main-drop select-store-dropdown">
-            <a href="javascript:void(0);" class="dropdown-toggle nav-link select-store"
-                data-bs-toggle="dropdown">
-                <span class="user-info">
-                    <span class="user-letter">
-                        <img src="{{ asset('assets/img/store/store-01.png') }}"
-                            alt="Store Logo" class="img-fluid">
-                    </span>
-                    <span class="user-detail">
-                        <span class="user-name">Select Store</span>
-                    </span>
-                </span>
-            </a>
-            <div class="dropdown-menu dropdown-menu-right">
-                <a href="javascript:void(0);" class="dropdown-item">
-                    <img src="https://dreamspos.dreamstechnologies.com/html/template/assets/img/store/store-01.png"
-                        alt="Store Logo" class="img-fluid"> Grocery Alpha
-                </a>
-                <a href="javascript:void(0);" class="dropdown-item">
-                    <img src="https://dreamspos.dreamstechnologies.com/html/template/assets/img/store/store-02.png"
-                        alt="Store Logo" class="img-fluid"> Grocery Apex
-                </a>
-                <a href="javascript:void(0);" class="dropdown-item">
-                    <img src="https://dreamspos.dreamstechnologies.com/html/template/assets/img/store/store-03.png"
-                        alt="Store Logo" class="img-fluid"> Grocery Bevy
-                </a>
-                <a href="javascript:void(0);" class="dropdown-item">
-                    <img src="https://dreamspos.dreamstechnologies.com/html/template/assets/img/store/store-04.png"
-                        alt="Store Logo" class="img-fluid"> Grocery Eden
-                </a>
-            </div>
-        </li> -->
-
-        <!-- <li class="nav-item dropdown has-arrow flag-nav nav-item-box">
-            <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="javascript:void(0);"
-                role="button">
-                <img src="https://dreamspos.dreamstechnologies.com/html/template/assets/img/flags/us.png"
-                    alt="Language" class="img-fluid">
-            </a>
-            <div class="dropdown-menu dropdown-menu-right">
-                <a href="javascript:void(0);" class="dropdown-item active">
-                    <img src="https://dreamspos.dreamstechnologies.com/html/template/assets/img/flags/us.png"
-                        alt height="16"> English
-                </a>
-                <a href="javascript:void(0);" class="dropdown-item">
-                    <img src="https://dreamspos.dreamstechnologies.com/html/template/assets/img/flags/fr.png"
-                        alt height="16"> French
-                </a>
-                <a href="javascript:void(0);" class="dropdown-item">
-                    <img src="https://dreamspos.dreamstechnologies.com/html/template/assets/img/flags/es.png"
-                        alt height="16"> Spanish
-                </a>
-                <a href="javascript:void(0);" class="dropdown-item">
-                    <img src="https://dreamspos.dreamstechnologies.com/html/template/assets/img/flags/de.png"
-                        alt height="16"> German
-                </a>
-            </div>
-        </li>
-
-        <li class="nav-item nav-item-box">
-            <a href="javascript:void(0);" id="btnFullscreen">
-                <i data-feather="maximize"></i>
-            </a>
-        </li> 
-        <li class="nav-item nav-item-box">
-            <a href="#">
-                <i data-feather="mail"></i>
-                <span class="badge rounded-pill">1</span>
-            </a>
-        </li>-->
-
-        <!-- <li class="nav-item dropdown nav-item-box">
-            <a href="javascript:void(0);" class="dropdown-toggle nav-link" data-bs-toggle="dropdown">
-                <i data-feather="bell"></i><span class="badge rounded-pill">2</span>
-            </a>
-            <div class="dropdown-menu notifications">
-                <div class="topnav-dropdown-header">
-                    <span class="notification-title">Notifications</span>
-                    <a href="javascript:void(0)" class="clear-noti"> Clear All </a>
-                </div>
-                <div class="noti-content">
-                    <ul class="notification-list">
-                        <li class="notification-message">
-                            <a href="#">
-                                <div class="media d-flex">
-                                    <span class="avatar flex-shrink-0">
-                                        <img alt
-                                            src="https://dreamspos.dreamstechnologies.com/html/template/assets/img/profiles/avatar-02.jpg">
-                                    </span>
-                                    <div class="media-body flex-grow-1">
-                                        <p class="noti-details"><span class="noti-title">John Doe</span> added
-                                            new task <span class="noti-title">Patient appointment
-                                                booking</span>
-                                        </p>
-                                        <p class="noti-time"><span class="notification-time">4 mins ago</span>
-                                        </p>
-                                    </div>
-                                </div>
-                            </a>
-                        </li>
-                        <li class="notification-message">
-                            <a href="#">
-                                <div class="media d-flex">
-                                    <span class="avatar flex-shrink-0">
-                                        <img alt
-                                            src="https://dreamspos.dreamstechnologies.com/html/template/assets/img/profiles/avatar-03.jpg">
-                                    </span>
-                                    <div class="media-body flex-grow-1">
-                                        <p class="noti-details"><span class="noti-title">Tarah
-                                                Shropshire</span>
-                                            changed the task name <span class="noti-title">Appointment booking
-                                                with payment gateway</span>
-                                        </p>
-                                        <p class="noti-time"><span class="notification-time">6 mins ago</span>
-                                        </p>
-                                    </div>
-                                </div>
-                            </a>
-                        </li>
-                        <li class="notification-message">
-                            <a href="#">
-                                <div class="media d-flex">
-                                    <span class="avatar flex-shrink-0">
-                                        <img alt
-                                            src="https://dreamspos.dreamstechnologies.com/html/template/assets/img/profiles/avatar-06.jpg">
-                                    </span>
-                                    <div class="media-body flex-grow-1">
-                                        <p class="noti-details"><span class="noti-title">Misty Tison</span>
-                                            added <span class="noti-title">Domenic Houston</span> and <span
-                                                class="noti-title">Claire Mapes</span> to project <span
-                                                class="noti-title">Doctor available module</span>
-                                        </p>
-                                        <p class="noti-time"><span class="notification-time">8 mins ago</span>
-                                        </p>
-                                    </div>
-                                </div>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="topnav-dropdown-footer">
-                    <a href="#">View all Notifications</a>
-                </div>
-            </div>
-        </li> -->
-
-        <!-- <li class="nav-item nav-item-box">
-            <a href="#"><i data-feather="settings"></i></a>
-        </li> -->
-        <a href="{{ route('pos-dashboard') }}" target="_blank"><strong>Open POS Dashboard</strong></a>
-
+        @if (auth()->user()->profile_image)
+            @php
+                $profile_picture = asset(auth()->user()->profile_image);
+            @endphp
+        @else
+            @php
+                $profile_picture = asset('assets/img/default-user.png');
+            @endphp
+        @endif
         <li class="nav-item dropdown has-arrow main-drop">
             <a href="javascript:void(0);" class="dropdown-toggle nav-link userset" data-bs-toggle="dropdown">
                 <span class="user-info">
                     <span class="user-letter">
-                        <img src="@if(auth()->user()->profile_image) {{env('APP_URL')}}/{{auth()->user()->profile_image}} @else https://dreamspos.dreamstechnologies.com/html/template/assets/img/profiles/avator1.jpg @endif"
-                            alt class="img-fluid">
+                        <img src="{{ $profile_picture }}" alt="" class="img-fluid">
                     </span>
                     <span class="user-detail">
                         <span class="user-name">{{ auth()->user()->name }}</span>
-                        <span class="user-role">Super Admin</span>
+                        <span class="user-role">{{ auth()->user()->getRoleNames()->first() }}</span>
                     </span>
                 </span>
             </a>
             <div class="dropdown-menu menu-drop-user">
                 <div class="profilename">
                     <div class="profileset">
-                        <span class="user-img"><img
-                                src="@if(auth()->user()->profile_image) {{env('APP_URL')}}/{{auth()->user()->profile_image}} @else https://dreamspos.dreamstechnologies.com/html/template/assets/img/profiles/avator1.jpg @endif"
-                                alt>
-                            <span class="status online"></span></span>
+                        <span class="user-img">
+                            <img src="{{ $profile_picture }}" alt="">
+                            <span class="status online"></span>
+                        </span>
                         <div class="profilesets">
                             <h6>{{ auth()->user()->name }}</h6>
-                            <h5>Super Admin</h5>
+                            <h5>{{ auth()->user()->getRoleNames()->first() }}</h5>
                         </div>
                     </div>
                     <hr class="m-0">
-                    <a class="dropdown-item"  href="{{ route('profile.index')}}"> <i class="me-2" data-feather="user"></i> My Profile</a>
-                    <a class="dropdown-item" href="#"><i class="me-2" data-feather="settings"></i>Settings</a>
+                    <a class="dropdown-item" href="{{ route('profile.index') }}"> 
+                        <i class="me-2" data-feather="user"></i>
+                        My Profile
+                    </a>
+                    <a class="dropdown-item" href="#">
+                        <i class="me-2" data-feather="settings"></i>
+                        Settings
+                    </a>
                     <hr class="m-0">
-                    <a class="dropdown-item logout pb-0" href="{{ route('logout') }}"><img
-                            src="{{ asset('assets/img/icons/log-out.svg') }}" class="me-2" alt="img">Logout</a>
+                    <a class="dropdown-item logout pb-0" href="{{ route('logout') }}">
+                        <img src="{{ asset('assets/img/icons/log-out.svg') }}" class="me-2" alt="">
+                        Logout
+                    </a>
                 </div>
             </div>
         </li>
