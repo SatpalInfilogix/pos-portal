253c86760ef1bceef4216df4097a72010433f228
SatpalInfilogix <satpalinfilogix@gmail.com>
2024-07-30 10:27:51 +0530

    Updated sales graph

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -61,22 +61,17 @@
             <div class="col-xl-8 col-sm-12 col-12 d-flex">
                 <div class="card flex-fill">
                     <div class="card-header d-flex justify-content-between align-items-center">
-                        <h5 class="card-title mb-0">Purchase & Sales</h5>
+                        <h5 class="card-title mb-0">Sales 2023-2024</h5>
                         <div class="graph-sets">
-                            <ul class="mb-0">
-                                <li>
-                                    <span>Sales</span>
-                                </li>
-                                <li>
-                                    <span>Purchase</span>
-                                </li>
-                            </ul>
                             <div class="dropdown dropdown-wraper">
                                 <button class="btn btn-light btn-sm dropdown-toggle" type="button" id="dropdownMenuButton"
                                     data-bs-toggle="dropdown" aria-expanded="false">
-                                    2023
+                                    2024
                                 </button>
                                 <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                    <li>
+                                        <a href="javascript:void(0);" class="dropdown-item">2024</a>
+                                    </li>
                                     <li>
                                         <a href="javascript:void(0);" class="dropdown-item">2023</a>
                                     </li>
