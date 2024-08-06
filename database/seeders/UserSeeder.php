8df82e591cb8bc7217e538e3025f7eedc2ed717d
SatpalInfilogix <satpalinfilogix@gmail.com>
2024-08-06 16:59:15 +0530

    Fixed super admin password seeding issue

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -22,7 +22,7 @@ class UserSeeder extends Seeder
             'first_name' => 'Super',
             'last_name' => 'Admin',
             'email' => 'superadmin@gmail.com',
-            'password' => Hash::make('123456'),
+            'password' => Hash::make('Admin@12345'),
             'email_verified_at' => now(),
             'remember_token' => Str::random(10)
         ]);
